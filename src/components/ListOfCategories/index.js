8b7d5dbd572a72f8a1b5cf3dfd4b945b763f328e
Jacke <ing.jacke@gmail.com>
2021-10-11 16:38:08 -0500

    Componente PhotoCards

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Category } from '.../Category'
+import { Category } from '../Category'
 
 import { List, Item } from './styles'
 import { categories } from '../../../api/db.json'
@@ -8,8 +8,7 @@ export const ListOfCategories = () => {
   return (
     <List>
       {
-            categories.map(category => <Item key={category.id}><Category {...category}
-            /></Item>)
+            categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
         }
     </List>
   )
